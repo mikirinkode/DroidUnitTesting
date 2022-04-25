4624ba8fce09c1ea588da23d8d8eacffa723b33d
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-04-25 17:10:29 +0700

    fix wrong logic

---
+++ app/src/main/java/com/mikirinkode/droidunittesting/video3/SampleFunction.kt
@@ -0,0 +1,48 @@
+package com.mikirinkode.droidunittesting.video3
+
+object SampleFunction {
+
+    /**
+     * Returns the n-th fibonacci number
+     * They are defined like this:
+     * fib(0) = 0
+     * fib(1) = 1
+     * fib(n) = fib(n-1) + fib(n-1)
+     */
+    fun fib(n: Int): Long {
+        if(n == 0 || n == 1) {
+            return n.toLong()
+        }
+        var a = 0L
+        var b = 1L
+        var c = 1L
+        (1..n-1).forEach { i ->
+            c = a + b
+            a = b
+            b = c
+        }
+        return c
+    }
+
+    /**
+     * Checks if the braces are set correctly
+     * e.g. "(a * b))" should return false
+     */
+    fun checkBraces(string: String): Boolean {
+        var firstBrace = 0
+        var lastBrace = 0
+        var result = false
+        if (string.count { it == '(' } == string.count { it == ')' }){
+            string.forEachIndexed { index, c ->
+                if (c == '('){
+                    firstBrace = index
+                } else if (c == ')') {
+                    lastBrace = index
+                }
+            }
+            result = firstBrace < lastBrace
+        }
+
+        return result
+    }
+}
--- app/src/test/java/com/mikirinkode/droidunittesting/video3/SampleFunctionTest.kt
+++ app/src/test/java/com/mikirinkode/droidunittesting/video3/SampleFunctionTest.kt
@@ -1,14 +1,21 @@
-package com.mikirinkode.droidunittesting
+package com.mikirinkode.droidunittesting.video3
 
 import com.google.common.truth.Truth.assertThat
+import com.mikirinkode.droidunittesting.video3.SampleFunction
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class SampleFunctionTest {
 
     @Test
+    fun `given number 7 will return 13`(){
+        val result = SampleFunction.fib(7)
+        assertEquals(13, result)
+    }
+
+    @Test
     fun `given number 8 will return 21`(){
-        val result = SampleFunction.fib(9)
+        val result = SampleFunction.fib(8)
         assertEquals(21, result)
     }
 
@@ -21,6 +28,18 @@ class SampleFunctionTest {
     @Test
     fun `another invalid braces return false`(){
         val result = SampleFunction.checkBraces("(Hello))")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `invalid braces order return false`(){
+        val result = SampleFunction.checkBraces("Hello)(")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `another invalid braces order return false`(){
+        val result = SampleFunction.checkBraces(")(Hello")
         assertThat(result).isFalse()
     }
 
